2105ccf9334153485bf1f7f16c3d295f12f9d6b9
ShivamShekhar1997 <iamshivamshekhar@gmail.com>
2023-05-12 15:30:24 +0530

    Added Jupyter notbooks

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
         if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            requirements.remove(HYPEN_E_DOT)  # To read just the library names excluding -e .
 
     return requirements        
 
